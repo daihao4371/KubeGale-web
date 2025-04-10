6a2318968095cfba37e593b12ff1215c6926dd9d
daihao4371 <daihao_4371@163.com>
2025-04-10 23:15:17 +0800

    feat: 优化角色管理页面

--- src/pages/system/roles/RoleManagement.vue
+++ src/pages/system/roles/RoleManagement.vue
@@ -30,8 +30,6 @@
           min-width="150"
         />
         
-
-        
         <!-- 创建时间列 -->
         <el-table-column
           prop="CreatedAt"
@@ -46,18 +44,12 @@
         <!-- 操作列 -->
         <el-table-column
           label="操作"
-          min-width="200"
+          min-width="380"
           fixed="right"
         >
           <template #default="scope">
             <div class="operation-buttons">
-              <el-button
-                type="primary"
-                link
-                @click="viewRoleDetail(scope.row)"
-              >
-                <el-icon><View /></el-icon>查看
-              </el-button>
+              <!-- 移除了查看按钮 -->
               
               <el-button
                 type="primary"
@@ -65,6 +57,33 @@
                 @click="editRole(scope.row)"
               >
                 <el-icon><Edit /></el-icon>编辑
+              </el-button>
+              
+              <!-- 新增的设置权限按钮 -->
+              <el-button
+                type="primary"
+                link
+                @click="showDevelopingMessage('设置权限')"
+              >
+                <el-icon><Setting /></el-icon>设置权限
+              </el-button>
+              
+              <!-- 新增的新增子角色按钮 -->
+              <el-button
+                type="primary"
+                link
+                @click="showDevelopingMessage('新增子角色')"
+              >
+                <el-icon><Plus /></el-icon>新增子角色
+              </el-button>
+              
+              <!-- 新增的拷贝按钮 -->
+              <el-button
+                type="primary"
+                link
+                @click="showDevelopingMessage('拷贝角色')"
+              >
+                <el-icon><CopyDocument /></el-icon>拷贝
               </el-button>
               
               <el-button
@@ -84,9 +103,10 @@
 
 <script lang="ts" setup>
 import { onMounted, computed } from 'vue'
-import { Plus, View, Edit, Delete } from '@element-plus/icons-vue'
+import { Plus, Edit, Delete, Setting, CopyDocument } from '@element-plus/icons-vue'
 import { useRoleManagement } from '../modules/roles/useRoleManagement'
 import { AuthorityData } from '@/api/system/roles/authority'
+import { ElMessage } from 'element-plus'
 
 // 使用角色管理逻辑
 const {
@@ -94,12 +114,17 @@ const {
   loading,
   tableConfig,
   fetchRoleList,
-  viewRoleDetail,
   editRole,
   deleteRole
 } = useRoleManagement()
 
-
+// 显示功能开发中的消息
+const showDevelopingMessage = (feature: string) => {
+  ElMessage({
+    message: `${feature}功能正在开发中，敬请期待！`,
+    type: 'info'
+  })
+}
 
 // 格式化日期
 const formatDate = (dateString: string) => {
@@ -128,4 +153,11 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 @import '../modules/roles/styles/roleStyles.scss';
+
+/* 调整操作按钮样式 */
+.operation-buttons {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+}
 </style>
